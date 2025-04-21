4f8be517d88ac740a189aec57bf1ec6d6891a1e7
fsgadari <franciscosgadari@gmail.com>
2025-04-21 13:55:23 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HealthProvider } from './context/HealthContext';
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
-import HealthForm from './pages/HealthForm'; // <- deve estar assim se for export default
+import HealthForm from './pages/HealthForm';
 import RecordsTable from './pages/RecordsTable';
+import { Toaster } from 'sonner'; // 👈 Importa o Toaster do sonner
 
 function App() {
   return (
     <HealthProvider>
       <Router>
+        {/* Toaster global para exibir popups */}
+        <Toaster position="top-center" richColors />
         <Layout>
           <Routes>
             <Route path="/" element={<Dashboard />} />
