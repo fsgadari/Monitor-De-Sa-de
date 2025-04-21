68fbb9e5bfc07468b0aee038e1766abaa8414f0d
fsgadari <franciscosgadari@gmail.com>
2025-04-21 01:44:21 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAIx1p2-tME0785U0VntX2FPoKsYie8fyQ",
@@ -11,6 +11,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true
+});
 
 export { db };
