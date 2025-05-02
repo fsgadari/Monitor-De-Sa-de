2385d81837fad48196860c2345a12e3edd1180ca
fsgadari <franciscosgadari@gmail.com>
2025-05-01 21:29:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HealthProvider } from './context/HealthContext';
 import Layout from './components/Layout';
+import Dashboard from './pages/Dashboard'; // <- Recoloque isso
 import HealthForm from './pages/HealthForm';
 import RecordsTable from './pages/RecordsTable';
 import { Toaster } from 'sonner';
@@ -13,9 +14,10 @@ function App() {
         <Toaster position="top-center" richColors />
         <Layout>
           <Routes>
-            <Route path="/" element={<HealthForm />} /> {/* <- Agora o formulário é a página inicial */}
+            <Route path="/" element={<HealthForm />} /> {/* Formulário é a página inicial */}
             <Route path="/form" element={<HealthForm />} />
             <Route path="/records" element={<RecordsTable />} />
+            <Route path="/dashboard" element={<Dashboard />} /> {/* <- Adicionado de volta */}
           </Routes>
         </Layout>
       </Router>
